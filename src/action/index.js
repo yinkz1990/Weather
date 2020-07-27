68a87535e8fc609c68f5c5b3a4f04240cf067f33
Idumu Olayinka Osayame <43887633+yinkz1990@users.noreply.github.com>
2020-07-27 11:40:13 +0100

    change the https request

--- src/action/index.js
+++ src/action/index.js
@@ -2,19 +2,13 @@ import axios from "axios";
 export const FETCH_URL = "FETCH_URL";
 
 const API_KEY = "5a2f7435806d120de8f9528fba33bd08";
-const rootUrl = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const rootUrl = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
-
-export default function fetchUrl (city){
-    const URL = `${rootUrl}&q=${city},us`;
-    const request = axios.get(URL);
-    return {
-        type: FETCH_URL,
-        payload: request
-
-    }
-        
-    
-
-};
-
+export default function fetchUrl(city) {
+  const URL = `${rootUrl}&q=${city},us`;
+  const request = axios.get(URL);
+  return {
+    type: FETCH_URL,
+    payload: request,
+  };
+}
