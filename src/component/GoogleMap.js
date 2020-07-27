2409faa91c3014357096fc3c080bf4ad2c970c28
Idumu Olayinka Osayame <43887633+yinkz1990@users.noreply.github.com>
2020-07-27 11:51:56 +0100

    added state name

--- src/component/GoogleMap.js
+++ src/component/GoogleMap.js
@@ -1,28 +1,28 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
-
-
+import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
 
 class GoogleMap extends Component {
-mapStyles = {
+  mapStyles = {
     height: "200px",
-    width: "250px"}
-    render() {
-        return (
-            <Map
-              google={this.props.google}
-              zoom={8}
-              style= {this.mapStyles}
-              initialCenter={{ lat: this.props.lat, lng: this.props.lon}}
-             >
-            <Marker position={{ lat: this.props.lat + 0.1, lng: this.props.lon + 0.1}}/>
-            </Map>
-            
-        );
-      }
+    width: "250px",
+  };
+  render() {
+    return (
+      <Map
+        google={this.props.google}
+        zoom={8}
+        style={this.mapStyles}
+        initialCenter={{ lat: this.props.lat, lng: this.props.lon }}
+      >
+        <Marker
+          position={{ lat: this.props.lat + 0.1, lng: this.props.lon + 0.1 }}
+        />
+      </Map>
+    );
+  }
 }
 
 export default GoogleApiWrapper({
-    apiKey: 'AIzaSyCIOp63pP1GzkZC0-_epgdGo_n-_4klflI'
-  })(GoogleMap);
+  apiKey: "AIzaSyCIOp63pP1GzkZC0-_epgdGo_n-_4klflI",
+})(GoogleMap);
--- src/container/WeatherList.js
+++ src/container/WeatherList.js
@@ -1,54 +1,55 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Chart from "../component/chart";
 import GoogleMap from "../component/GoogleMap";
-import {connect} from "react-redux";
+import { connect } from "react-redux";
 
-
-class WeatherList extends Component{
-
-    
-    renderWeather = (cityData) => {
-        if(!cityData){
-            return null;
-        }
-        const name = cityData.city.name;
-        const temps = cityData.list.map(weather => weather.main.temp);
-        const humid = cityData.list.map(weather => weather.main.humidity);
-        const pressure = cityData.list.map(weather => weather.main.pressure);
-        const {lon, lat} = cityData.city.coord;
-        
-        return(
-            <tr key = {name} >
-                <td><GoogleMap lat = {lat} lon = {lon}/></td>
-                <td><Chart data = {temps} color = "red" unit ="K" /></td>
-                <td><Chart data = {pressure} color = "green" unit ="hPa"/></td>
-                <td><Chart data = {humid} color = "blue" unit ="%"/></td>
-            </tr>
-
-        );
-
+class WeatherList extends Component {
+  renderWeather = (cityData) => {
+    if (!cityData) {
+      return null;
     }
-    render(){
-        return(
-            <table className = "table table-hover">
-                <thead>
-                    <tr>
-                        <th>City</th>
-                        <th>Temperature(K)</th>
-                        <th>Pressure(hPa)</th>
-                        <th>Humidity(%)</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {this.props.weather.map(this.renderWeather)}
-                </tbody>
-            </table>
+    const name = cityData.city.name;
+    const temps = cityData.list.map((weather) => weather.main.temp);
+    const humid = cityData.list.map((weather) => weather.main.humidity);
+    const pressure = cityData.list.map((weather) => weather.main.pressure);
+    const { lon, lat } = cityData.city.coord;
 
-        );
-    }
+    return (
+      <tr key={name}>
+        <td>
+          <GoogleMap lat={lat} lon={lon} />
+          <p>{name}</p>
+        </td>
+        <td>
+          <Chart data={temps} color="red" unit="K" />
+        </td>
+        <td>
+          <Chart data={pressure} color="green" unit="hPa" />
+        </td>
+        <td>
+          <Chart data={humid} color="blue" unit="%" />
+        </td>
+      </tr>
+    );
+  };
+  render() {
+    return (
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>City</th>
+            <th>Temperature(K)</th>
+            <th>Pressure(hPa)</th>
+            <th>Humidity(%)</th>
+          </tr>
+        </thead>
+        <tbody>{this.props.weather.map(this.renderWeather)}</tbody>
+      </table>
+    );
+  }
 }
-const mapStateToProps = ({weather}) => {
-    return { weather};
-}
+const mapStateToProps = ({ weather }) => {
+  return { weather };
+};
 
 export default connect(mapStateToProps)(WeatherList);
